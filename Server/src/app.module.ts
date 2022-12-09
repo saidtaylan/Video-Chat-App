e02eff262f3231d5944bd6c138479fadd057530a
Muhammed Said Taylan <saidtaylann@gmail.com>
2022-12-09 17:06:27 +0300

    docker server

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -6,9 +6,15 @@ import {AuthModule} from './auth/auth.module';
 import {RoomModule} from './room/room.module';
 import {WsModule} from './ws/ws.module';
 import { AppController } from './app.controller';
+import {ServeStaticModule} from "@nestjs/serve-static"
+import { join } from 'path';
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'Server'),
+            exclude: []
+        }),
         UserModule,
         ConfigModule.forRoot({isGlobal: true}),
         MongooseModule.forRoot(process.env.DATABASE_URL),
